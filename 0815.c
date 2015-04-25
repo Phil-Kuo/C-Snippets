93fedbdbf5f44f374a4426ec992bd55f5bcb3b14
ym <yeemin.kuo@gmail.com>
2015-04-25 11:05:37 +0800

    call an end for now

---
+++ 0815.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int
+main (int argc, char *argv[])
+{
+	printf("Enter message to be encrypted: ");
+	
+	return 0;
+}
--- 0817.c
+++ 0817.c
@@ -2,15 +2,30 @@
 
 #define N 99
 
+void create_magic_square_one(int magic_square[][N], int n);
+void create_magic_square_two(int magic_square[][N], int n);
+void print_magic_square(int magic_square[][N], int n);
+
 int
 main (int argc, char *argv[])
 {
-	int n, magic_square[N][N] = {0}, i, j, s;
+	int n, magic_square[N][N] = {0};
 	
 	printf("This programe creates a magic square of a specified size.\n"
 		   "The size must be an odd number between 1 and 99.\n"
 		   "Enter size of magic square: ");
 	scanf("%d", &n);
+	
+	create_magic_square_one(magic_square, n); /*adopt first method*/
+	print_magic_square(magic_square, n);
+	
+	return 0;
+}
+
+void
+create_magic_square_one(int magic_square[][N], int n)
+{
+	int i, j, s;
 	
 	for (i = 0, j = n/2, s = 1; s <= n * n; s++)
 	{
@@ -46,7 +61,12 @@ main (int argc, char *argv[])
 		}
 		magic_square[i--][j++] = s;
 	}
-	
+}
+
+void
+print_magic_square(int magic_square[][N], int n)
+{
+	int i, j;
 	for (i = 0; i < n; i++)
 	{
 		for (j = 0; j < n; j++)
@@ -55,5 +75,4 @@ main (int argc, char *argv[])
 		}
 		printf("\n");
 	}
-	return 0;
 }
