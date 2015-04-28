e4a15c83d4e7527337ea3a643847fd21384af0f3
ym <yeemin.kuo@gmail.com>
2015-04-28 18:39:18 +0800

    rename 1307.c->1306.c

--- 0511.c
+++ 0511.c
@@ -7,16 +7,24 @@ main (int argc, char *argv[])
     
     int number, first_digit, second_digit;
     
+    char *first_d[] = {"twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+    char *second_d[] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    char *special[] = {"ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+    
     printf("Enter a two-digit number: ");
     scanf("%d", &number);
     
     first_digit = number / 10;
     second_digit = number % 10;
     
-    switch(first_digit){
-        case 0: case 1:
-            break;
-        case 2: 
-    
+    if (first_digit == 1)
+    {
+    	printf("%s\n", special[second_digit]);
+    }
+    else
+    {
+    	printf("%s-%s\n", first_d[first_digit-2], second_d[second_digit-1]);
+    }
     return 0;
 }
+
--- 1307.c
+++ 1307.c
@@ -1,48 +1,30 @@
 #include <stdio.h>
-#include <ctype.h>
-
-#define NUM_PLANETS 9
-
-int string_equal(char *s, char *t);
 
 int
 main (int argc, char *argv[])
 {
-	char *planets[] = {"Mercury", "Venus", "Earth", 
-					   "Mars", "Junipter", "Saturn",
-					   "Uranus", "Nepture", "Pluto"};
-					  
-	int i, j;
-	
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; j < NUM_PLANETS; j++)
-		{
-			if (string_equal(argv[i], planets[j]) == 0)
-			{
-				printf("%s is planet %d\n", argv[i], j + 1);
-				break;
-			}
-		}
-		if (j == NUM_PLANETS)
-		{
-			printf("%s is not the planet\n", argv[i]);
-		}
-	}
-	return 0;
+/*    输入两位数，显示其英文单词*/
+    
+    int number, first_digit, second_digit;
+    
+    char *first_d[] = {"twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+    char *second_d[] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    char *special[] = {"ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+    
+    printf("Enter a two-digit number: ");
+    scanf("%d", &number);
+    
+    first_digit = number / 10;
+    second_digit = number % 10;
+    
+    if (first_digit == 1)
+    {
+    	printf("%s\n", special[second_digit]);
+    }
+    else
+    {
+    	printf("%s-%s\n", first_d[first_digit-2], second_d[second_digit-1]);
+    }
+    return 0;
 }
 
-int 
-string_equal(char *s, char *t)
-{
-	int i;
-	
-	for (i = 0; toupper(s[i]) == toupper(t[i]); i++)
-	{
-		if (s[i] == '\0')
-		{			
-			return 1;
-		}
-	}
-	return 0;
-}
